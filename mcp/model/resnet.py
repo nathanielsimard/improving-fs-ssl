6f8ccaf54872f868ab1be597a9c2284cfbf329b7
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2020-10-25 10:05:13 -0400

    Updated ResNet model definition (#16)

--- mcp/model/resnet.py
+++ mcp/model/resnet.py
@@ -1,13 +1,28 @@
 import torch
-from torchvision.models.resnet import BasicBlock, ResNet
+from torch import nn
+from torchvision.models.resnet import BasicBlock, Bottleneck, ResNet
 
 from mcp.model.base import Model
 
 
-class ResNet18(Model):
-    def __init__(self, embed_size: int):
+class _ResNet(Model):
+    def __init__(self, resnet: ResNet, embed_size: int):
         super().__init__()
-        self.model = ResNet(BasicBlock, [2, 2, 2, 2], embed_size)
+        self.encoder = resnet
+        self.output = nn.Linear(embed_size, embed_size)
+        self.relu = nn.ReLU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.model(x)
+        x = self.encoder(x)
+        x = self.relu(x)
+        return self.output(x)
+
+
+class ResNet18(_ResNet):
+    def __init__(self, embed_size: int):
+        super().__init__(ResNet(BasicBlock, [2, 2, 2, 2], embed_size), embed_size)
+
+
+class ResNet50(_ResNet):
+    def __init__(self, embed_size: int):
+        super().__init__(ResNet(Bottleneck, [3, 4, 6, 3], embed_size), embed_size)
