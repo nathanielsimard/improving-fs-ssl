1376d253adb29cc489ce882f4760ebb8ebf59699
nathaniel <nathaniel.simard.42@gmail.com>
2020-10-28 14:19:04 -0400

    Debug

--- mcp/training/trainer.py
+++ mcp/training/trainer.py
@@ -70,8 +70,17 @@ class Trainer(object):
 
         for epoch in range(starting_epoch + 1, self.epochs + 1):
             self._training_phase(epoch)
+
+            train_optimizer_state = self.optimizer_train.state_dict()
+            train_scheduler_state = self.scheduler_train.state_dict()
+            train_model_state = self.model.state_dict()
+
             self._training_support_phase(epoch)
             self._evaluation_phase(epoch)
+
+            self.optimizer_train.load_state_dict(train_optimizer_state)
+            self.scheduler_train.load_state_dict(train_scheduler_state)
+            self.model.load_state_dict(train_model_state)
 
     def _training_phase(self, epoch):
         self.training_loop.fit_one(
