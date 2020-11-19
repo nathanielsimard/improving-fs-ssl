66263cb3bf459d49ce7d9a6b9679097d03afe402
nathaniel <nathaniel.simard.42@gmail.com>
2020-11-18 19:15:21 -0500

    Test

--- mcp/task/rotation.py
+++ mcp/task/rotation.py
@@ -65,7 +65,7 @@ class RotationTask(Task):
         x, y = self.batch_rotation.rotate(x)
         x = encoder(x)
         x = self.output(x)
-        print(self.output.requires_grad)
+        print(self.output.parameters()[0].requires_grad)
 
         metric = self.metric(x, y)
         loss = self.loss(x, y)
