5e2d73dc324261f7e6573d190b41b274e9872037
nathaniel <nathaniel.simard.42@gmail.com>
2020-11-18 19:28:48 -0500

    Debug

--- mcp/data/dataset/cifar.py
+++ mcp/data/dataset/cifar.py
@@ -137,7 +137,7 @@ class CifarFsDataset(Dataset):
         self.transform = transform
 
     def __len__(self):
-        return 0.1 * len(self.dataset)
+        return int(0.1 * len(self.dataset))
 
     def __getitem__(self, index: int):
         img, label = self.dataset[index]
