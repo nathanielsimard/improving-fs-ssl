1f945523181c53636792bb9bd44e58b5ff56d71a
laggui <lagrange.guillaume.1@gmail.com>
2020-12-22 12:46:00 -0500

    Update ResNet-12 to have fixed embedding size w/ adaptive pooling

--- mcp/model/resnet_12.py
+++ mcp/model/resnet_12.py
@@ -117,7 +117,7 @@ class ResNet(nn.Module):
             block_size=dropblock_size,
         )
         if avg_pool:
-            self.avgpool = nn.AvgPool2d(5, stride=1)
+            self.avgpool = nn.AdaptiveAvgPool2d(1)
         self.keep_prob = keep_prob
         self.keep_avg_pool = avg_pool
         self.dropout = nn.Dropout(p=1 - self.keep_prob, inplace=False)
